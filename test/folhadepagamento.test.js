07249fa21a702c132a9720fc54e590cc5520bc56
= <=>
2024-08-28 23:53:16 -0300

    Reports

--- test/folhadepagamento.test.js
+++ test/folhadepagamento.test.js
@@ -1,17 +1,19 @@
-import { somaHorasExtras, calculaDescontos } from "../index.js";
+import { somaHorasExtras, calculaDescontos } from "../index.js"
 
-// eslint-disable-next-line no-undef
-test('Deve retornar a soma das horas extras', () => {
-    const esperado = 2500;
-    const retornado = somaHorasExtras(2000, 500);
+describe('Testes dos calculos de folha', () => {
     // eslint-disable-next-line no-undef
-    expect(retornado).toBe(esperado);
-});
-  
-// eslint-disable-next-line no-undef
-test('Deve descontar o valor do salario', () => {
-    const esperado = 2300;
-    const retornado = calculaDescontos(2500, 200);
+    it('Deve retornar a soma das horas extras', () => {
+        const esperado = 2500
+        const retornado = somaHorasExtras(2000, 500)
+        // eslint-disable-next-line no-undef
+        expect(retornado).toBe(esperado)
+    });
+    
     // eslint-disable-next-line no-undef
-    expect(retornado).toBe(esperado);
-});
+    it('Deve descontar o valor do salario', () => {
+        const esperado = 2300
+        const retornado = calculaDescontos(2500, 200)
+        // eslint-disable-next-line no-undef
+        expect(retornado).toBe(esperado)
+    })
+})
